217ed70e41d621f1d681780026d3c25c3512a56e
Matt <luismatheus.v@gmail.com>
2019-12-10 15:29:41 -0300

    initial commit

---
+++ readme.md
@@ -0,0 +1,12 @@
+*Internship Project
+
+#Filterfy is a simple for for GitHub issues!
+Filterfy's a small project written in JavaScript made to interact with GitHub's API! It transcribes the issues of any specific repository into a .CSV file. 
+
+#Filterfy é um filtro simples para issues do GitHub!
+Trata-se de um pequeno projeto de integração com o API do GitHub escrito em JavaScript. Ele transcreve as issues de um repositório específico para um arquivo .CSV.
+
+All it takes is inputting the URL of a desired GitHub repository, and Filterfy 
+
+#Filterfy est un logiciel simple pour tes issues GitHub! 
+Filterfy est un projet simple écrit en JavaScript, fait pour interagir avec l'API GitHub! Il transcrit les issues en fichiers .CSV.
---
+++ src/filter.js
@@ -0,0 +1,57 @@
+'use strict';
+//Filesystem module 
+const fs = require('fs');
+//Fetch module
+const fetch = require('node-fetch');
+//Readline module
+const readline = require('readline').createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+//Gets the user's URL and validates it
+const filterfy = readline.question(`Please insert the full URL of the desired repository: `, (repo) => {
+	let testURL = repo.substr(0, 18);
+	if(testURL === `https://github.com`) {
+		let validURL = repo.substr(19, );
+		let gitRepo = `https://api.github.com/repos/${validURL}/issues?=page=1&per_page=100`;
+	//Uses the user's URL to fetch the info from the API and turns it into a JS object array
+		async function getGit() {
+			let gitInfo;
+			let response = await fetch(gitRepo);
+			gitInfo = await response.text();
+			let objection = JSON.parse(gitInfo);
+		//Filtering the content of the array. 
+			let issues = "title― body― state― number― assignee― labels― milestone\n";
+			objection.filter(function(finalData) {
+			//Makes sure we get the correct data for the Assignee login and Milestone Title.
+				let assigneeLogin = finalData.assignee;
+				if(assigneeLogin != null && "login" in assigneeLogin) assigneeLogin = assigneeLogin.login;
+				let milestoneTitle = finalData.milestone;
+				if(milestoneTitle != null && "title" in milestoneTitle) milestoneTitle = milestoneTitle.title;
+			//Organizes the array into readable CSV content content.
+				issues += finalData.title.replace(/"/g, "'");
+				issues += "―" + finalData.body.replace(/(\r\n|\n|\r)/gm, "").replace(/"/g, "'")
+				issues += "―" + finalData.state;
+				issues += "―" + finalData.number;
+				issues += "―" + assigneeLogin;
+				issues += "―" + JSON.stringify(finalData.labels); //Turns the "labels" vector into a JS string/object.
+				issues += "―" + milestoneTitle;
+				issues += "\n"
+			});
+	//If the operation was succesful, this will download the CSV file.
+			fs.writeFile("../issues.csv", issues, function(err) {
+				if(err) {
+					return console.log(err);
+				}
+				console.log("Your issues have been saved!", 'You can find your file in the Filterfy folder.', 'Bye!');
+			});
+		}
+		getGit();
+	}
+	//In case the user's URL doesn't match the expected parameters.
+	else {
+		console.log('Please insert a valid URL for a GitHub repository')
+	}
+	readline.close();
+});
+filterfy;
