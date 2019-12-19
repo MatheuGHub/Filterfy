23a2aa13014ab92aa5760e370434a04d432c0e43
MatheuGHub <luismatheus.v@gmail.com>
2019-12-19 16:27:57 -0300

    Update filter.js

--- src/filter.js
+++ src/filter.js
@@ -13,15 +13,15 @@ const filterfy = readline.question(`Please insert the full URL of the desired re
 	let inputURL = repo.substr(0, 18);
 	if(inputURL === `https://github.com`) {
 		let validUserURL = repo.substr(19, );
-		let gitRepo = `https://api.github.com/repos/${validUserURL}/issues?=page=1&per_page=100`;
+		let gitRepository = `https://api.github.com/repos/${validUserURL}/issues?=page=1&per_page=100`;
 	//Uses the user's URL to fetch the info from the API and turns it into a JS object array
 		async function getGitInfo() {
 			let gitInfo;
-			let fetchResponse = await fetch(gitRepo);
+			let fetchResponse = await fetch(gitRepository);
 			gitInfo = await fetchResponse.text();
 			let makeJsonObject = JSON.parse(gitInfo);
 		//Filtering the content of the array. 
-			let issues = "title― body― state― number― assignee― labels― milestone\n";
+			let repositoryIssues = "title― body― state― number― assignee― labels― milestone\n";
 			makeJsonObject.filter(function(finalData) {
 			//Makes sure we get the correct data for the Assignee login and Milestone Title.
 				let assigneeLogin = finalData.assignee;
@@ -29,14 +29,14 @@ const filterfy = readline.question(`Please insert the full URL of the desired re
 				let milestoneTitle = finalData.milestone;
 				if(milestoneTitle != null && "title" in milestoneTitle) milestoneTitle = milestoneTitle.title;
 			//Organizes the array into readable CSV content content.
-				issues += finalData.title.replace(/"/g, "'");
-				issues += "―" + finalData.body.replace(/(\r\n|\n|\r)/gm, "").replace(/"/g, "'")
-				issues += "―" + finalData.state;
-				issues += "―" + finalData.number;
-				issues += "―" + assigneeLogin;
-				issues += "―" + JSON.stringify(finalData.labels); //Turns the "labels" vector into a JS string/object.
-				issues += "―" + milestoneTitle;
-				issues += "\n"
+				repositoryIssues += finalData.title.replace(/"/g, "'");
+				repositoryIssues += "―" + finalData.body.replace(/(\r\n|\n|\r)/gm, "").replace(/"/g, "'")
+				repositoryIssues += "―" + finalData.state;
+				repositoryIssues += "―" + finalData.number;
+				repositoryIssues += "―" + assigneeLogin;
+				repositoryIssues += "―" + JSON.stringify(finalData.labels); //Turns the "labels" vector into a JS string/object.
+				repositoryIssues += "―" + milestoneTitle;
+				repositoryIssues += "\n"
 			});
 	//If the operation was succesful, this will download the CSV file.
 			fs.writeFile("../issues.csv", issues, function(err) {
